5c5c3dd23142158e12b3c901f3c5825b5d99ec15
patrickhbarfield <patrickhbarfield@gmail.com>
2022-11-23 22:27:16 -0800

    initialize java cardgame

---
+++ card app/crazy8s.java
@@ -0,0 +1,10 @@
+public class crazy8s {
+    private int rank;
+    private int suite;
+}
+
+    public crazy8s(int rank, int suite) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
